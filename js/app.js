2c59e4f5fe6bbf48dbf23a16436305112335f4ff
Ju Liu <ju.liu@welaika.com>
2014-05-28 09:30:14 +0100

    Add products listing

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ App = Ember.Application.create({
 App.Router.map(function() {
   this.route('about');
   this.route('credits', { path: '/thanks' });
+  this.resource('products');
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -14,3 +15,26 @@ App.IndexController = Ember.Controller.extend({
     return (new Date()).toDateString();
   }.property()
 });
+
+App.PRODUCTS = [
+  {
+    title: "Flint",
+    price: 99,
+    description: "Flint is good for your skin",
+    isOnSale: true,
+    image: "images/products/flint.png"
+  },
+  {
+    title: "Kindling",
+    price: 99,
+    description: "Easily kindling is very good for your health",
+    isOnSale: false,
+    image: "images/products/kindling.png"
+  }
+];
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
+})
