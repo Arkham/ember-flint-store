1f24b89c95bff1da73fea73c8ceb8bee3e310b85
Ju Liu <ju.liu@welaika.com>
2014-05-28 12:11:13 +0100

    Add contacts and switch to store and fixtures

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ App.Router.map(function() {
   this.route('about');
   this.route('credits', { path: '/thanks' });
   this.resource('products');
+  this.resource('product', { path: "/products/:product_id" });
+  this.resource('contacts');
+  this.resource('contact', { path: "/contacts/:contact_id" });
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -16,8 +19,37 @@ App.IndexController = Ember.Controller.extend({
   }.property()
 });
 
-App.PRODUCTS = [
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('product');
+  }
+})
+
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('contact');
+  }
+})
+
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('boolean'),
+  image: DS.attr('string')
+});
+
+App.Contact = DS.Model.extend({
+  name: DS.attr('string'),
+  about: DS.attr('string'),
+  avatar: DS.attr('string')
+})
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Product.FIXTURES = [
   {
+    id: 1,
     title: "Flint",
     price: 99,
     description: "Flint is good for your skin",
@@ -25,6 +57,7 @@ App.PRODUCTS = [
     image: "images/products/flint.png"
   },
   {
+    id: 2,
     title: "Kindling",
     price: 99,
     description: "Easily kindling is very good for your health",
@@ -33,8 +66,17 @@ App.PRODUCTS = [
   }
 ];
 
-App.ProductsRoute = Ember.Route.extend({
-  model: function() {
-    return App.PRODUCTS;
+App.Contact.FIXTURES = [
+  {
+    id: 1,
+    name: "Ju",
+    avatar: "images/contacts/giamia.png",
+    about: "Hell Boy"
+  },
+  {
+    id: 2,
+    name: "Gio",
+    avatar: "images/contacts/anostagia.png",
+    about: "Yolo Yolo"
   }
-})
+]
